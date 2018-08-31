19b3e8572d45d5f4aa92480965de5b056a59c107
xanxus1111 <809339558@qq.com>
2018-08-31 13:44:00 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -6,13 +6,18 @@ use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
-    public function created(Reply $reply)
-    {
-        $reply->topic->increment('reply_count', 1);
-    }
+  public function created(Reply $reply)
+  {
+      $topic = $reply->topic;
+      $topic->increment('reply_count', 1);
+
+      // 通知作者话题被回复了
+      $topic->user->notify(new TopicReplied($reply));
+  }
 
     public function creating(Reply $reply)
     {

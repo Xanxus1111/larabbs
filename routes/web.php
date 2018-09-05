fe0f1f7ed0680726158cf3b2b2d62ee72a1aa3cb
xanxus1111 <809339558@qq.com>
2018-09-05 06:11:53 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@
 //
 // Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
